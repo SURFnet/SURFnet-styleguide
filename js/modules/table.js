919b5a2135fb081cf18b9e9f1343a91d6044e91f
Arjan Eising <arjan@arjaneising.nl>
2012-10-09 14:17:15 +0200

    Lots of CSS fixes for mobile

--- js/modules/table.js
+++ js/modules/table.js
@@ -1,9 +1,14 @@
 var app = app || {};
 
-app.table = function(){
+app.table = function() {
+    var ranResponsiveTables = false;
+
 
     var init = function() {
         initDataTable();
+        responsiveDataTable();
+
+        $(window).on('resize', responsiveDataTable);
     };
 
 
@@ -19,6 +24,44 @@ app.table = function(){
                 }
             });
         });
+    };
+
+
+    var responsiveDataTable = function() {
+        var tables = $('.data-table-wrapper table');
+
+        if (tables.length) {
+            if ($(window).width() < 767) {
+                tables.addClass('narrow');
+            }
+            else {
+                tables.removeClass('narrow');
+            }
+
+            if (!ranResponsiveTables) {
+                ranResponsiveTables = true;
+
+                tables.each(function() {
+                    var table = $(this),
+                        headers = [],
+                        tr;
+
+                    table.find('thead th').each(function() {
+                        headers.push($(this).text());
+                    });
+
+                    table.find('tr').each(function() {
+                        tr = $(this);
+
+                        tr.find('td').each(function(index, td) {
+                            td = $(td);
+
+                            td.attr('data-title', headers[index]);
+                        });
+                    });
+                });
+            }
+        }
     };
 
 
