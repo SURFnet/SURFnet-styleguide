d215908efb800be90b43614f5cf37d78195a7c39
Arjan Eising <a.eising@flex.surfnet.nl>
2012-11-27 10:01:43 +0100

    Backport of latest Distribution channel changes

--- redesign/js/modules/app-grid.js
+++ redesign/js/modules/app-grid.js
@@ -1,7 +1,9 @@
 var app = app || {};
 
 app.appgrid = function() {
-    var gridElm;
+    var gridElm, keywords;
+
+    keywords = [];
 
     var init  = function() {
         gridElm = $('.app-grid');
@@ -22,7 +24,7 @@ app.appgrid = function() {
             var elm = $(this),
                 target = $(e.target);
 
-            if (target.is('a')) {
+            if (target.is('a') || target.closest('a').length === 1) {
                 return;
             }
 
@@ -36,55 +38,175 @@ app.appgrid = function() {
 
 
     var setSearch = function() {
-        var placeholder = gridElm.data('searchPlaceholder');
+        var placeholderText = app.message.i18n('appgrid.search.placeholder'),
+            filters = '';
 
-        gridElm.before('<input type="search" class="app-grid-search" placeholder="' + placeholder + '">');
+        if (gridElm.hasClass('filters-available')) {
+            filters = '<div>' +
+                        '<ul>';
+            if (gridElm.hasClass('lmng-active')) {
+                filters += '<li><a href="#" data-filter="licensed">' + app.message.i18n('appgrid.filter.license') + '</a></li>' +
+                           '<li><a href="#" data-filter="not-licensed">' + app.message.i18n('appgrid.filter.no-license') + '</a></li>';
+            }
+            filters += '<li><a href="#" data-filter="connected">' + app.message.i18n('appgrid.filter.connected') + '</a></li>' +
+                       '<li><a href="#" data-filter="not-connected">' + app.message.i18n('appgrid.filter.not-connected') + '</a></li>' +
+                      '</ul>' +
+                    '</div>';
+        }
+
+        gridElm.before('<nav class="filter-grid' + (gridElm.hasClass('filters-available') ? ' filters-available' : '') + '">' +
+                           '<input type="search" class="app-grid-search" placeholder="' + placeholderText + '">' +
+                           filters +
+                       '</nav>');
 
         var searchElm = $('.app-grid-search'),
-            timer = null;
+            filterLinks = $('.filter-grid a'),
+            timer = null,
+            activeFilters = [];
+
+
+        function checkFocusKeyUp(e) {
+            if (e.which === 191 && !$(e.target).is('input,textarea,a,submit,button')) {
+                searchElm[0].focus();
+            }
+        }
+
 
         function setTimer() {
             if (timer) {
                 clearTimeout(timer);
             }
-            timer = setTimeout(doSearch, 100);
+            timer = setTimeout(doSearch, 250);
         }
 
-        function doSearch() {
-            var isSearch = searchElm.val().length !== 0,
-                keywords = [];
+
+        function doSearch(time) {
+            var isSearch = searchElm.val().length !== 0;
+
+            if (time === undefined) {
+                time = 250;
+            }
+            
+            keywords = [];
 
             if (isSearch) {
                 keywords = searchElm.val().toLowerCase().split(' ');
             }
 
-            gridElm.find('li').each(function(index, elm) {
-                var $elm = $(elm),
-                    display = true,
-                    text = elm.textContent.toLowerCase() || elm.innerText.toLowerCase();
-
-                $.each(keywords, function(i, kw) {
-                    if (display === false) {
-                        return;
-                    }
-
-                    display = false;
-
-                    if (text.indexOf(kw) !== -1) {
-                        display = true;
-                    }
-                });
-
-                if (display) {
-                    $elm.removeClass('hide');
-                }
-                else {
-                    $elm.addClass('hide');
-                }
+            gridElm.tickback('filter', {
+                duration: time
             });
         }
 
+
+        function doFilter(e) {
+            e.preventDefault();
+
+            var clickedFilter = $(this),
+                theFilter = clickedFilter.data('filter'),
+                index = $.inArray(theFilter, activeFilters);
+
+            if (index === -1) {
+                activeFilters.push(theFilter);
+                clickedFilter.addClass('active-filter');
+            }
+            else {
+                activeFilters.splice(index, 1);
+                clickedFilter.removeClass('active-filter');
+            }
+
+            doSearch(250);
+        }
+
+
+        function mustDisplay(elm) {
+            var display = true,
+                text = elm.text().toLowerCase();
+
+            $.each(keywords, function(i, kw) {
+                if (display === false) {
+                    return;
+                }
+
+                display = false;
+
+                if (text.indexOf(kw) !== -1) {
+                    display = true;
+                }
+            });
+
+            filters = ignoreObsoleteFilters(activeFilters);
+
+            $.each(filters, function(i, filter) {
+                if (display === false) {
+                    return;
+                }
+
+                display = false;
+
+                if (elm.hasClass(filter)) {
+                    display = true;
+                }
+            });
+
+            return !display;
+        }
+
+
+        function ignoreObsoleteFilters(filters) {
+            var cantHaveBoth = [['connected', 'not-connected'], ['licensed', 'not-licensed']],
+                inspecting = null,
+                pos0, pos1,
+                toReturn = $.extend(true, [], filters);
+
+            for (var l = cantHaveBoth.length - 1; l >= 0; --l) {
+                inspecting = cantHaveBoth[l];
+
+                pos0 = $.inArray(inspecting[0], toReturn);
+                pos1 = $.inArray(inspecting[1], toReturn);
+
+                if (pos0 !== -1 && pos1 !== -1) {
+                    toReturn.splice(pos0, 1);
+                    toReturn.splice($.inArray(inspecting[1], toReturn), 1);
+                }
+            }
+
+            return toReturn;
+        }
+
+
+        function howToSort(a, b){
+            var aText = $.trim(a.find('h2').text().toLowerCase()),
+                bText = $.trim(b.find('h2').text().toLowerCase());
+
+            if (aText < bText) {
+                  return -1;
+            }
+            if (aText > bText) {
+                return 1;
+            }
+            return 0;
+        }
+
+
         searchElm.bind('keyup change', setTimer);
+        filterLinks.on('click', doFilter);
+        $(window).keyup(checkFocusKeyUp);
+
+
+        gridElm.tickback({
+            activeItemsFirst: true,
+            duration: 250,
+            filterCallback: mustDisplay,
+            itemInactiveClass: 'grid-item-hidden',
+            itemInactiveStyles: {
+                opacity: 0.35
+            },
+            itemActiveStyles: {
+                opacity: 1
+            },
+            sortCallback: howToSort
+        });
     };
 
 
